b22b8a764e4314393ede395e8d8b554e9866b978
ttyngai <ttyngai@gmail.com>
2021-12-15 14:13:08 -0800

    header Footer

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// This will load our env variables
+require('dotenv').config();
+
+// This will connect us to the database
+require('./config/database');
+
 var app = express();
 
 // view engine setup
@@ -23,12 +29,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
