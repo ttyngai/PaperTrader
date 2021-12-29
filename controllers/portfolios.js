11b2bd12e9f40f863aa12120679e356761a52661
ttyngai <ttyngai@gmail.com>
2021-12-29 01:44:46 -0800

    Add Exchange Rate USDCAD easter Egg

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -86,6 +86,14 @@ function show(req, res) {
             p.shares;
         }
       });
+      //Find "USDCAD=X"
+      let usdCad;
+      prices.forEach(function (p) {
+        if (p.symbol === 'USDCAD=X') {
+          usdCad = p.preRegAfterCombinedPrice;
+        }
+      });
+
       res.render(`portfolios/show`, {
         title: 'Portfolios',
         portfolio,
@@ -95,6 +103,7 @@ function show(req, res) {
         realizedPL,
         totalHoldings,
         dayPL,
+        usdCad,
       });
     });
   });
